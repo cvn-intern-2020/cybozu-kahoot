550900c34fcb9a442ec597f8a0646e7e930b9ec4
Des <dtennos@yahoo.com>
2020-08-10 07:28:31 +0700

    Added quiz editor

---
+++ frontend/src/components/QuizEdit/QuestionList.jsx
@@ -0,0 +1,238 @@
+import React from 'react';
+import { useFieldArray } from 'react-hook-form';
+import Tab from 'react-bootstrap/Tab';
+import Nav from 'react-bootstrap/Nav';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+const QuestionList = ({ control, register }) => {
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: 'questions',
+    });
+    return (
+        <Tab.Container defaultActiveKey="questions[0].number">
+            <Col sm={2}>
+                <Button
+                    onClick={() => {
+                        let maxQuestionNumber = 1;
+                        fields.forEach((ques) =>
+                            ques.number > maxQuestionNumber
+                                ? (maxQuestionNumber = ques.number)
+                                : null
+                        );
+                        append({
+                            number: maxQuestionNumber + 1,
+                            title: '',
+                            type: 'single',
+                            timeLimit: 15,
+                            point: 2000,
+                            answers: [
+                                {
+                                    id: 1,
+                                    title: '',
+                                },
+                                {
+                                    id: 2,
+                                    title: '',
+                                },
+                                {
+                                    id: 3,
+                                    title: '',
+                                },
+                                {
+                                    id: 4,
+                                    title: '',
+                                },
+                            ],
+                            correctAnswers: [1],
+                        });
+                    }}
+                >
+                    Add question
+                </Button>
+                <Nav variant="pills" className="flex-column">
+                    {fields.map((item, index) => {
+                        return (
+                            <Nav.Item key={item.number}>
+                                <Row>
+                                    <Nav.Link
+                                        eventKey={`questions[${index}].number`}
+                                    >
+                                        Question {item.number}
+                                    </Nav.Link>
+                                    <Button
+                                        type="button"
+                                        variant="danger"
+                                        onClick={() => remove(index)}
+                                    >
+                                        X
+                                    </Button>
+                                </Row>
+                            </Nav.Item>
+                        );
+                    })}
+                </Nav>
+            </Col>
+            <Col sm={10}>
+                <Tab.Content>
+                    {fields.map((item, index) => {
+                        return (
+                            <Tab.Pane
+                                key={item.number}
+                                eventKey={`questions[${index}].number`}
+                            >
+                                <Row>
+                                    <Form.Control
+                                        type="text"
+                                        name={`questions[${index}].title`}
+                                        ref={register()}
+                                        defaultValue={item.title}
+                                        placeholder="Enter quiz title..."
+                                    />
+                                </Row>
+                                <Row>
+                                    <Col sm={6}></Col>
+                                    <Col>
+                                        <Row>
+                                            <Form.Control
+                                                type="number"
+                                                name={`questions[${index}].timeLimit`}
+                                                ref={register()}
+                                                defaultValue={item.timeLimit}
+                                                placeholder="Enter time limit..."
+                                            />
+                                        </Row>
+                                        <Row>
+                                            <Form.Control
+                                                type="number"
+                                                name={`questions[${index}].point`}
+                                                ref={register()}
+                                                defaultValue={item.point}
+                                                placeholder="Enter question max points..."
+                                            />
+                                        </Row>
+                                        <Row>
+                                            <Form.Control
+                                                as="select"
+                                                name={`questions[${index}].type`}
+                                                ref={register()}
+                                                defaultValue={item.point}
+                                            >
+                                                <option value="single">
+                                                    Single
+                                                </option>
+                                                <option value="multi">
+                                                    Multi
+                                                </option>
+                                            </Form.Control>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <InputGroup className="mb-3">
+                                            <Form.Control
+                                                type="text"
+                                                name={`questions[${index}].answers[0].title`}
+                                                ref={register()}
+                                                defaultValue={
+                                                    item.answers[0].title
+                                                }
+                                                placeholder="Enter 1st answer..."
+                                            />
+                                            <InputGroup.Append>
+                                                <input
+                                                    type="checkbox"
+                                                    name={`questions[${index}].answers[0].correct`}
+                                                    ref={register()}
+                                                    defaultChecked={
+                                                        item.answers[0].correct
+                                                    }
+                                                />
+                                            </InputGroup.Append>
+                                        </InputGroup>
+                                    </Col>
+                                    <Col>
+                                        <InputGroup className="mb-3">
+                                            <Form.Control
+                                                type="text"
+                                                name={`questions[${index}].answers[1].title`}
+                                                ref={register()}
+                                                defaultValue={
+                                                    item.answers[1].title
+                                                }
+                                                placeholder="Enter 1st answer..."
+                                            />
+                                            <InputGroup.Append>
+                                                <input
+                                                    type="checkbox"
+                                                    name={`questions[${index}].answers[1].correct`}
+                                                    ref={register()}
+                                                    defaultChecked={
+                                                        item.answers[1].correct
+                                                    }
+                                                />
+                                            </InputGroup.Append>
+                                        </InputGroup>
+                                    </Col>
+                                    <Col>
+                                        <InputGroup className="mb-3">
+                                            <Form.Control
+                                                type="text"
+                                                name={`questions[${index}].answers[2].title`}
+                                                ref={register()}
+                                                defaultValue={
+                                                    item.answers[2].title
+                                                }
+                                                placeholder="Enter 1st answer..."
+                                            />
+                                            <InputGroup.Append>
+                                                <input
+                                                    type="checkbox"
+                                                    name={`questions[${index}].answers[2].correct`}
+                                                    ref={register()}
+                                                    defaultChecked={
+                                                        item.answers[2].correct
+                                                    }
+                                                />
+                                            </InputGroup.Append>
+                                        </InputGroup>
+                                    </Col>
+                                    <Col>
+                                        <InputGroup className="mb-3">
+                                            <Form.Control
+                                                type="text"
+                                                name={`questions[${index}].answers[3].title`}
+                                                ref={register()}
+                                                defaultValue={
+                                                    item.answers[3].title
+                                                }
+                                                placeholder="Enter 1st answer..."
+                                            />
+                                            <InputGroup.Append>
+                                                <input
+                                                    type="checkbox"
+                                                    name={`questions[${index}].answers[3].correct`}
+                                                    ref={register()}
+                                                    defaultChecked={
+                                                        item.answers[3].correct
+                                                    }
+                                                />
+                                            </InputGroup.Append>
+                                        </InputGroup>
+                                    </Col>
+                                </Row>
+                            </Tab.Pane>
+                        );
+                    })}
+                </Tab.Content>
+            </Col>
+        </Tab.Container>
+    );
+};
+
+export default QuestionList;
--- frontend/src/components/QuizEdit/QuizEdit.jsx
+++ frontend/src/components/QuizEdit/QuizEdit.jsx
@@ -1,93 +1,122 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import QuizEditNavbar from './QuizEditNavbar/QuizEditNavbar';
-import QuestionList from './QuestionList/QuestionList';
-import QuestionDetail from './QuestionDetail/QuestionDetail';
 import Config from '../../config';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+import { useForm } from 'react-hook-form';
 import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Col from 'react-bootstrap/Col';
 import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import QuestionList from './QuestionList';
 
 const QuizEdit = () => {
     const { quizId } = useParams();
-    const [currentQuestion, setCurrentQuestion] = useState({
-        number: 1,
-        title: '',
-        type: 'single',
-        timeLimit: 20,
-        point: 2000,
-        answers: [],
-        correctAnswers: [],
-    });
-    const [quiz, setQuiz] = useState({
-        title: '',
-        questions: [currentQuestion],
+    const { register, control, handleSubmit, getValues, reset } = useForm({
+        defaultValues: {
+            title: '',
+            questions: [
+                {
+                    number: 1,
+                    title: '',
+                    type: 'single',
+                    timeLimit: 15,
+                    point: 2000,
+                    answers: [
+                        {
+                            id: 1,
+                            title: '',
+                            correct: true,
+                        },
+                        {
+                            id: 2,
+                            title: '',
+                            correct: false,
+                        },
+                        {
+                            id: 3,
+                            title: '',
+                            correct: false,
+                        },
+                        {
+                            id: 4,
+                            title: '',
+                            correct: false,
+                        },
+                    ],
+                },
+            ],
+        },
     });
 
-    useEffect(() => {
-        fetch(`${Config.backendURL}/api/quiz/${quizId}`, {
-            credentials: 'include',
-        })
-            .then((res) => res.json())
-            .then((foundedQuiz) => {
-                setQuiz(foundedQuiz);
-                setCurrentQuestion(foundedQuiz.questions[0]);
-            });
-    }, []);
-
-    const changeQuestionNumber = (questionNumber) => {
-        setCurrentQuestion(
-            quiz.questions.find((q) => q.number === questionNumber)
-        );
-    };
-    const changeQuizTitle = (quizTitle) =>
-        setQuiz({ ...quiz, title: quizTitle });
-    const removeQuestion = (questionNumber) => {
-        if (currentQuestion.number === questionNumber) changeQuestionNumber(1);
-        let { questions } = quiz;
-        const questionIndex = questions.findIndex(
-            (q) => q.number === questionNumber
-        );
-        questions.splice(questionIndex, 1);
-        setQuiz({ ...quiz, questions });
-    };
-    const updateQuiz = () => {
+    const onSubmit = (data) => {
+        for (let i = 0; i < data.questions.length; i++) {
+            data.questions[i].number = i + 1;
+            data.questions[i].correctAnswers = [];
+            for (let j = 0; j < data.questions[i].answers.length; j++) {
+                data.questions[i].answers[j].id = j + 1;
+                if (data.questions[i].answers[j].correct)
+                    data.questions[i].correctAnswers.push(j + 1);
+            }
+        }
         fetch(`${Config.backendURL}/api/quiz/${quizId}`, {
             credentials: 'include',
             method: 'put',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(quiz),
+            body: JSON.stringify(data),
         });
     };
 
+    useEffect(() => {
+        async function fetchData() {
+            const data = await fetch(
+                `${Config.backendURL}/api/quiz/${quizId}`,
+                {
+                    credentials: 'include',
+                }
+            ).then((res) => res.json());
+            for (let i = 0; i < data.questions.length; i++) {
+                for (let j = 0; j < data.questions[i].answers.length; j++) {
+                    if (
+                        data.questions[i].correctAnswers.findIndex(
+                            (a) => a === data.questions[i].answers[j].id
+                        ) !== -1
+                    )
+                        data.questions[i].answers[j].correct = true;
+                    else data.questions[i].answers[j].correct = false;
+                }
+            }
+            reset(data);
+        }
+        fetchData();
+    }, [reset]);
+
     return (
-        <div>
-            <QuizEditNavbar
-                quiz={quiz}
-                onChange={changeQuizTitle}
-                onSubmit={updateQuiz}
-            />
-            <Row>
-                <Col md={3} lg={2}>
-                    <Button variant="success" block>
-                        Add question
-                    </Button>
-                    <QuestionList
-                        questions={quiz.questions}
-                        changeQuestionNumber={changeQuestionNumber}
-                        activeQuestionNumber={currentQuestion.number}
-                        removeQuestion={removeQuestion}
-                    ></QuestionList>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Row>
+                <Col xs={10}>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Prepend>
+                            <InputGroup.Text>Quiz Title</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                            placeholder="Quiz title"
+                            name="title"
+                            ref={register({ required: true })}
+                        />
+                    </InputGroup>
                 </Col>
                 <Col>
-                    <QuestionDetail question={currentQuestion} />
+                    <Button variant="primary" block type="submit">
+                        Done
+                    </Button>
                 </Col>
-            </Row>
-        </div>
+            </Form.Row>
+            <Form.Row>
+                <QuestionList {...{ control, register, getValues }} />
+            </Form.Row>
+        </Form>
     );
 };
 
