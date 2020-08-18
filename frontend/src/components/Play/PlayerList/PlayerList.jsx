bc496cb000d63fede7633908646378ee39e13306
Pham Tran Trong Duy <dtennos@yahoo.com>
2020-08-18 16:35:32 +0700

    Changed element tag

--- frontend/src/components/Play/PlayerList/PlayerList.jsx
+++ frontend/src/components/Play/PlayerList/PlayerList.jsx
@@ -13,20 +13,21 @@ const PlayerList = ({ pin, players, onStart }) => {
             <Card className={`${styles.card} shadow-lg mb-4`}>
                 <Card.Body className="d-flex flex-column">
                     <p className={`${styles.title} text-center`}>PIN: {pin}</p>
-                    <Card.Text className={styles.text}>
-                        <p>Players: {players.length}</p>
-                        {players.length > 0
-                            ? players.map((player) => (
-                                  <Badge
-                                      variant={keyToVariants(player.number)}
-                                      key={player.number}
-                                      className={`${styles.badge} mr-1`}
-                                  >
-                                      {player.nickname}
-                                  </Badge>
-                              ))
-                            : null}
-                    </Card.Text>
+                    <div className={`${styles.text}`}>
+                        <span>Players: {players.length}</span>
+                        <div>
+                            {players.length > 0 &&
+                                players.map((player) => (
+                                    <Badge
+                                        variant={keyToVariants(player.number)}
+                                        key={player.number}
+                                        className={`${styles.badge} mr-1`}
+                                    >
+                                        {player.nickname}
+                                    </Badge>
+                                ))}
+                        </div>
+                    </div>
                     {pin && onStart && players.length > 0 && (
                         <Button
                             variant="danger"
